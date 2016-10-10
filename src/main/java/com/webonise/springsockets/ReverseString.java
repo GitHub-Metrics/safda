4458c53d14b79972cac55045b9bc82f110a36509
deeps-webonise <deepn94@gmail.com>
2016-10-10 14:07:55 +0530

    Added String reverse assignment using sockets

---
+++ src/main/java/com/webonise/springsockets/ReverseString.java
@@ -0,0 +1,18 @@
+package com.webonise.springsockets;
+
+public class ReverseString {
+
+    private String name;
+
+    public ReverseString() {
+    }
+
+    public ReverseString(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return name;
+    }
+
+}
